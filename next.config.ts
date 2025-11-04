131c55c558bfe5e30a167cafa2757b25eebc722a
BasavarajuVB <bbasavaraju3893@gmail.com>
2025-11-04 10:47:59 +0530

    Update Next.js configuration with redirects and rewrites; modify app metadata and remove favicon

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        { source: "/login", destination: "/pages/PageLogin" },
+        { source: "/home", destination: "/pages/PageHome" },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
 };
 
 export default nextConfig;
